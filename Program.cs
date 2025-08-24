0441991473003631a79cd2dec42fc4d914e2b35b
Eduardo Mendes <emendes@alunos.utfpr.edu.br>
2025-08-24 19:00:15 -0300

    Commit do Progam.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace InterpolacaoStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string nomePessoa;
+
+            nomePessoa = Console.ReadLine();
+            Console.WriteLine($"Seja Bem-Vindo {nomePessoa}");
+        }
+    }
+ }
